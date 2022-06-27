f98097359337f6739513170a84ac9e0a27e51edd
KengWMS <kengzila@gmail.com>
2022-06-27 10:27:03 +0700

    Test

--- app/main.js
+++ app/main.js
@@ -4,39 +4,37 @@ require("chromedriver");
 var should = require('chai').should();
 
 //desctible
-describe("add todo test", function () {
+describe("Test Log In", function () {
 
     //it block
-    it("Successfully adds a todo o application", async function () {
+    it("log in Success", async function () {
 
         //open browser
         let driver = await new Builder().forBrowser("chrome").build();
 
 
         //navigate to our browser
-        await driver.get("https://lambdatest.github.io/sample-todo-app/")
+        await driver.get("http://203.151.136.18/SahaTH_BD_WMS/#/login")
 
-        //add a todo
-        await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+//         //add a todo
+//         await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
 
-        //assert
-        let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function (value) {
-            return value
-        })
+//         //assert
+//         let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function (value) {
+//             return value
+//         })
 
-        //assert using chai should
-        todoText.should.equal("Learn Selenium");
-
-
-        //close browser
-        await driver.quit();
-//ghghghghghg
-
-    });
-
-});
+//         //assert using chai should
+//         todoText.should.equal("Learn Selenium");
 
 
+//         //close browser
+//         await driver.quit();
+// //ghghghghghg
+
+//     });
+
+// });
 
 
 
@@ -47,35 +45,37 @@ describe("add todo test", function () {
 
 
 
-///////////////////////////////////////////////////////////
 
 
-// async function example() {
-
-//     //open browser
-//     let driver = await new Builder().forBrowser("chrome").build();
+// ///////////////////////////////////////////////////////////
 
 
-//     //navigate to our browser
-//     await driver.get("https://lambdatest.github.io/sample-todo-app/")
+// // async function example() {
 
-//     //add a todo
-//     await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
-
-//     //assert
-//     let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function (value) {
-//         return value
-//     })
-
-//     //assert using node assertion
-//     assert.strictEqual(todoText, "Learn Selenium");
-
-//     //assert using chai should
-//     todoText.should.equal("Learn Selenium");
+// //     //open browser
+// //     let driver = await new Builder().forBrowser("chrome").build();
 
 
-//     //close browser
-//     await driver.quit();
+// //     //navigate to our browser
+// //     await driver.get("https://lambdatest.github.io/sample-todo-app/")
+
+// //     //add a todo
+// //     await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+// //     //assert
+// //     let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function (value) {
+// //         return value
+// //     })
+
+// //     //assert using node assertion
+// //     assert.strictEqual(todoText, "Learn Selenium");
+
+// //     //assert using chai should
+// //     todoText.should.equal("Learn Selenium");
 
 
-// }
+// //     //close browser
+// //     await driver.quit();
+
+
+// // }
